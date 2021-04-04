5cdeb4363208a986b7bde7ae857850fcee943936
Julio Cefnar Auyon R <juliorecinos30@gmail.com>
2021-04-03 23:55:56 -0600

    14 Conditionals

---
+++ Shared/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace InterviewApp_1.Shared.Entities
+{
+     public class Movie
+    {
+        public string Title { get; set; } 
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
